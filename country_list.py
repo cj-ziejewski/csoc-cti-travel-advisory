07c97afc23dd3b46b4cb85b5b7434a5f3bd94960
CJ Ziejewski <christopher.j.ziejewski@huntington.com>
2025-04-12 14:24:56 -0400

    Dummy change to retest Grype permissions

--- country_list.py
+++ country_list.py
@@ -1,12 +1,12 @@
-import requests
 import json
 import sqlite3
 import datetime
+import requests
 
-baseUrl = "https://cadataapi.state.gov/api/TravelAdvisories" 
+BASEURL = "https://cadataapi.state.gov/api/TravelAdvisories"
 
-def loadDoSTravel(baseUrl):
-    resp = requests.get(baseUrl, verify=False)
+def loadDoSTravel(BASEURL):
+    resp = requests.get(BASEURL, verify=False)
     dosRaw =  resp.text
     return dosRaw
 
@@ -18,33 +18,65 @@ def decodeTravel(dosRaw):
     return advisories
 
 def splitTravelAdvisory(advisoryLine):
-    advisory = []
-    timestamp = []
     elements = advisoryLine.split('\n')
-    advisory = elements[0]
-    timestamp = elements[1]
-    return advisory, timestamp
+    return elements
 
 def dbInit():
     try:
         dbConn = sqlite3.connect('travel.db')
         cursor = dbConn.cursor()
-        table = """ CREATE TABLE IF NOT EXISTS advisories (
+        table = """ CREATE TABLE IF NOT EXISTS travel (
                     datetimeUpdated VARCHAR(25) NOT NULL,
                     datetimeAdded VARCHAR(25) NOT NULL,
-                    threatLevel VARCHAR(7) NOT NULL,
-                    country VARCHAR(128) NOT NULL
+                    advisory VARCHAR(128) NOT NULL
                     ); """
         cursor.execute(table)
     except sqlite3.Error as error:
         return error
     finally:
         if dbConn:
-            return cursor
+            return dbConn, cursor
+
+def dbAddEntry(dbConn, cursor, advisory, timestamp, currentTime):
+    try:
+        query = """INSERT INTO travel
+                    VALUES (
+                    {},
+                    {},
+                    {}
+                    );
+                """.format(str(currentTime),str(timestamp),str(advisory))
+        cursor.execute(query)
+        dbConn.commit()
+    except sqlite3.Error as error:
+        return error
+
+def sanityCheckQuery(dbConn, cursor):
+    try:
+        query = """SELECT advisory FROM travel WHERE advisory IS NOT NULL"""
+        output = cursor.execute(query)
+    except sqlite3.Error as error:
+        return error
+    finally:
+        if dbConn:
+            dbConn.commit()
+            return output
 
 def dateConvert():
     currentTime = datetime.datetime.now()
     currentTime = currentTime.strftime("%Y-%m-%dT%H:%M:%S-04:00")
-    return(str(currentTime))
-if __name__ == '__main__': 
-    print(dateConvert())
+    return str(currentTime)
+
+if __name__ == '__main__':
+    dbObjects = dbInit()
+    advisoriesList = decodeTravel(dosRaw=loadDoSTravel(BASEURL=BASEURL))
+    for i in advisoriesList:
+        timestamp = splitTravelAdvisory(advisoryLine=i)[1]
+        advisory = splitTravelAdvisory(advisoryLine=i)[0]
+        dbAddEntry(dbConn=dbObjects[0],cursor=dbObjects[1],timestamp=timestamp, advisory=advisory, currentTime=dateConvert())
+    checkContents = sanityCheckQuery(dbConn=dbObjects[0],cursor=dbObjects[1])
+    for row in checkContents:
+        print(row)
+
+
+### Dummy change to re-test Grype
