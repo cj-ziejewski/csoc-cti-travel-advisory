5b238cbab07c948fcd21f06b7309334bb4d0a100
CJ Ziejewski <christopher.j.ziejewski@huntington.com>
2025-04-12 11:40:29 -0400

    Further entry parsing and initial bootstrapping for SQLite DB

--- country_list.py
+++ country_list.py
@@ -1,17 +1,50 @@
 import requests
 import json
+import sqlite3
+import datetime
 
 baseUrl = "https://cadataapi.state.gov/api/TravelAdvisories" 
 
 def loadDoSTravel(baseUrl):
     resp = requests.get(baseUrl, verify=False)
     dosRaw =  resp.text
-    return(dosRaw)
+    return dosRaw
 
-def decodeTravel(raw):
-    jsList = json.loads(raw)
-    for key in jsList:
-        print(key["Title"]+'\n'+key["Updated"])
+def decodeTravel(dosRaw):
+    jsonList = json.loads(dosRaw)
+    advisories = []
+    for key in jsonList:
+        advisories.append(key["Title"]+'\n'+key["Updated"])
+    return advisories
 
+def splitTravelAdvisory(advisoryLine):
+    advisory = []
+    timestamp = []
+    elements = advisoryLine.split('\n')
+    advisory = elements[0]
+    timestamp = elements[1]
+    return advisory, timestamp
+
+def dbInit():
+    try:
+        dbConn = sqlite3.connect('travel.db')
+        cursor = dbConn.cursor()
+        table = """ CREATE TABLE IF NOT EXISTS advisories (
+                    datetimeUpdated VARCHAR(25) NOT NULL,
+                    datetimeAdded VARCHAR(25) NOT NULL,
+                    threatLevel VARCHAR(7) NOT NULL,
+                    country VARCHAR(128) NOT NULL
+                    ); """
+        cursor.execute(table)
+    except sqlite3.Error as error:
+        return error
+    finally:
+        if dbConn:
+            return cursor
+
+def dateConvert():
+    currentTime = datetime.datetime.now()
+    currentTime = currentTime.strftime("%Y-%m-%dT%H:%M:%S-04:00")
+    return(str(currentTime))
 if __name__ == '__main__': 
-    decodeTravel(raw=loadDoSTravel(baseUrl=baseUrl))
+    print(dateConvert())
